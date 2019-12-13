180e1bbd136f70005abdcca79a09802e15f34014
Student <student@Makerss-MacBook-Pro.local>
2019-12-13 16:46:22 +0000

    energy usage

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -86,4 +86,53 @@ describe('when power saving mode is off', function() {
       }
       expect(thermostat.getCurrentTemperature()).toEqual(32);
     });
-  });
+});
+
+    describe('displaying usage levels', function() {
+    describe('when the temperature is below 18 degrees', function() {
+
+        var thermostat;
+
+        beforeEach(function() {
+        thermostat = new Thermostat();
+        });
+
+      it('it is considered low-usage', function() {
+        for (var i = 0; i < 3; i++) {
+          thermostat.down();
+        }
+        expect(thermostat.energyUsage()).toEqual('low-usage');
+      });
+    });
+  
+    describe('when the temperature is between 18 and 25 degrees', function() {
+
+            var thermostat;
+
+
+            beforeEach(function() {
+            thermostat = new Thermostat();
+            });
+
+            it('it is considered medium-usage', function() {
+            expect(thermostat.energyUsage()).toEqual('medium-usage');
+            });
+    });
+  
+    describe('when the temperature is anything else', function() {
+
+        var thermostat;
+
+        beforeEach(function() {
+        thermostat = new Thermostat();
+        });
+
+      it('it is considered high-usage', function() {
+        thermostat.powerSavingMode = false;
+        for (var i = 0; i < 6; i++) {
+          thermostat.up();
+        };
+        expect(thermostat.energyUsage()).toEqual('high-usage');
+        });
+    });
+});
